63d69647df5cf5a57a401dad781795b73b4a0c9b
Ghanishtha12 <kapoorghanishtha2005@gmail.com>
2026-07-28 23:09:53 +0530

    Setup Google AdSense verification

--- jobNext/src/components/common/AdPlaceholder.jsx
+++ jobNext/src/components/common/AdPlaceholder.jsx
@@ -1,29 +1,31 @@
 import { useEffect } from "react";
 
 export default function AdPlaceholder() {
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.src =
-      "https://pl30560395.effectivecpmnetwork.com/e7129ad472d074ec7e3d6bf0c075fa9e/invoke.js";
-    script.async = true;
-    script.setAttribute("data-cfasync", "false");
-
-    const container = document.getElementById(
-      "container-e7129ad472d074ec7e3d6bf0c075fa9e"
-    );
-
-    if (container && !container.querySelector("script")) {
-      container.appendChild(script);
-    }
-  }, []);
-
-  return (
-    <div
-      id="container-e7129ad472d074ec7e3d6bf0c075fa9e"
-      style={{
-        width: "100%",
-        margin: "20px auto",
-      }}
-    ></div>
-  );
+  return null;
 }
+//   useEffect(() => {
+//     const script = document.createElement("script");
+//     script.src =
+//       "https://pl30560395.effectivecpmnetwork.com/e7129ad472d074ec7e3d6bf0c075fa9e/invoke.js";
+//     script.async = true;
+//     script.setAttribute("data-cfasync", "false");
+
+//     const container = document.getElementById(
+//       "container-e7129ad472d074ec7e3d6bf0c075fa9e"
+//     );
+
+//     if (container && !container.querySelector("script")) {
+//       container.appendChild(script);
+//     }
+//   }, []);
+
+//   return (
+//     <div
+//       id="container-e7129ad472d074ec7e3d6bf0c075fa9e"
+//       style={{
+//         width: "100%",
+//         margin: "20px auto",
+//       }}
+//     ></div>
+//   );
+// }
